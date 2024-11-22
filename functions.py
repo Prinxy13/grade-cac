75acd38672d108b9dc33d79f3d57dc8ea420f49c
prinxy13 <davidngwu272@gmail.com>
2024-11-22 13:15:03 +0100

    done

--- functions.py
+++ functions.py
@@ -1,2 +1,12 @@
-Hello='say_hello(prince):'
-print("Hello!")
+def Name():
+    Name = input('Enter your name!:')
+    print(f'hello! {Name}')
+
+Name()
+
+
+def Goods(name, ammount):
+    print(f'hello! {name}. Do you want {ammount} eggs?')
+
+
+Goods('mummy jireh', '5')
