b5c1ead4adab0157425a3580a8279c30cf4071cb
prinxy13 <davidngwu272@gmail.com>
2024-11-25 11:11:19 +0100

    done by prinxy

--- BMI CALCULATOR.py
+++ BMI CALCULATOR.py
@@ -1,2 +1,18 @@
 Name = input('Enter your name pls!:')
 weight = int(input(f'your weightin kilograms is!:'))
+if weight <= 0:
+    print('Error! invalid value enterd!')
+
+hieght = float(input(f'your height in meters is:'))
+if height <= 0:
+    print('Error! Invalid value enered!')
+
+body_mass_index = weight/hieght**2
+if body_mass_index < 18.5:
+    print(f'{name}, your body mass index is{body_mass_index:.2f}\nYou are underweight')
+elif body_mass_index >= 18.5 and body_mass_index <  25:
+    print(f'{name}, your body mass index is {body_mass_index:.2f}\nYou have a normal weight')
+elif body_mass_index >= 25 and body_mass_index< 30:
+     print(f'{name}, your body mass index is {body_mass_index:.2f}\nopps!! You are overweight')
+elif body_mass_index >=30:
+     print(f'{name}, your body mass index is {body_mass_index:.2f}\nsorry!!, you are obese')
--- grade_cac.py
+++ grade_cac.py
@@ -1,16 +1,20 @@
-a ='welcome to my crypto hub!'
-Student_name = input('Enter your name:')
-print (f'Hola! {Student_name}! {a}') 
-exam_score = float(input('Enter your score:'))
-if exam_score < 0 or exam_score > 100:
-    print('error!!'); print('you are a faliour')
-elif exam_score >= 90 and exam_score <= 100:
-    print(f'hi {Student_name} you scored {exam_score} You got an A! Congratulations')
-elif exam_score >= 80 and exam_score <= 89:
-    print(f'hi {Student_name} you scored {exam_score} You made a B! Well done')
-elif exam_score >= 70 and exam_score <= 79:
-    print(f'hi {Student_name} you scored {exam_score} You made a C! keep it up')
-elif exam_score >= 60 and exam_score <= 69:
-    print(f'hi {Student_name} you scored {exam_score} You made a D! quit play and read your books')
-else:
-    print(' You made an F! you are a big faliour go and start selling fish')
+def  a():
+    a ='welcome to my crypto hub!'
+    Student_name = input('Enter your name:')
+    print (f'Hola! {Student_name}! {a}') 
+    exam_score = float(input('Enter your score:'))
+    if exam_score < 0 or exam_score > 100:
+        print('error!!'); print('you are a faliour')
+    elif exam_score >= 90 and exam_score <= 100:
+        print(f'hi {Student_name} you scored {exam_score} You got an A! Congratulations')
+    elif exam_score >= 80 and exam_score <= 89:
+        print(f'hi {Student_name} you scored {exam_score} You made a B! Well done')
+    elif exam_score >= 70 and exam_score <= 79:
+        print(f'hi {Student_name} you scored {exam_score} You made a C! keep it up')
+    elif exam_score >= 60 and exam_score <= 69:
+        print(f'hi {Student_name} you scored {exam_score} You made a D! quit play and read your books')
+    else:
+        print(' You made an F! you are a big faliour go and start selling fish')
+
+
+a()
